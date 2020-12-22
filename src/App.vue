86a02482af28d1ec892aa3a0a2b9a85fce2c4bd1
Prosanta <prosantapaulbsmrstu@gmail.com>
2020-12-22 12:39:21 +0600

    search and css commot

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,85 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <main>
+      <div class="search-box">
+        <input type="text" class="search-bar" placeholder="search..."/>
+      </div>
+      <div class="weather-wrap">
+        <div class="location-box">
+          <div class="location">Northamton,UK</div>
+          <div class="date">Monday 20 January 2020</div>
+        </div>
+        <div class="weather-box"></div>
+         <div class="temp">9</div>
+         <div class="weather">Rain</div>
+
+
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      api_key:'cc3775a4885d016ddba6e6e388c35ec9'
+    }
   }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
+body {
+  font-family: 'montserrat', sans-serif;
+}
+#app {
+  background-image: url('./assets/cold-bg.jpg');
+  background-size: cover;
+  background-position:bottom;
+  transition: 0.4s;
+}
+main {
+  min-height: 100vh;
+  padding: 25px;
+
+  background-image: linear-gradient(to bottom,rgba(0,0,0,0.25), rgba(0,0,0,0.75));
+}
+.search-box{
+  width: 100%;
+  margin-bottom: 30px;
+}
+.search-box .search-bar{
+  display:block;
+  width: 100%;
+  padding: 15px;
+
+  color: #313131;
+  font-size: 20px;
+
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25) ;
+  background-color: rgba(255,255,255,0.5);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+.search-box .search-bar:focus{
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25) ;
+  border-color: rgba(255,255,255,0.75);
+  border-radius: 16px 0px 16px  0px;
+}
+
+
 </style>
